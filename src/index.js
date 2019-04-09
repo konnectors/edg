2df73f220dde495903158181014179bf1418b3f5
LucsT <lucas@cozycloud.cc>
2019-04-09 17:32:19 +0200

    chore: Dummy commit to trigger build and fix icon

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ module.exports = new BaseKonnector(start)
 
 // The start function is run by the BaseKonnector instance only when it got all the account
 // information (fields). When you run this connector yourself in "standalone" mode or "dev" mode,
-// the account information come from ./konnector-dev-config.json file
+// the account information come from ./konnector-dev-config.json file.
 async function start(fields) {
   const token = await authenticate(fields.login, fields.password)
 
